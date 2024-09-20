3e6eaf783691ea2b84b6a9df00ed28f868e33edc
abhishek-end <abhishekyadav49534@gmail.com>
2024-09-20 14:19:53 +0530

    configure backend

--- api/app.js
+++ api/app.js
@@ -26,9 +26,10 @@ app.use("/", transactionRouter);
 
 // Error-handling middleware
 app.use(errorHandle);
+module.exports = app;
 
 // Start the server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
